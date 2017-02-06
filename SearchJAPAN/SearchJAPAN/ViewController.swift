4eabb949d8ed4b3c7ceee2e888d81a5ea3f0ce50
Yuuki Watanabe <yukiwata@yahoo-corp.jp>
2017-02-06 18:49:18 +0900

    xcodeでのプロジェクト作成

---
+++ SearchJAPAN/SearchJAPAN/ViewController.swift
@@ -0,0 +1,78 @@
+//
+//  ViewController.swift
+//  SearchJAPAN
+//
+//  Created by 渡辺　雄貴 on 2017/02/06.
+//  Copyright © 2017年 渡辺　雄貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // ステータスバーの高さ
+    let statusBarHeight = UIApplication.shared.statusBarFrame.height
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // UITableView を作成
+        let tableView = UITableView()
+        
+        // サイズと位置調整
+        tableView.frame = CGRect(
+            x: 0,
+            y: statusBarHeight,
+            width: self.view.frame.width,
+            height: self.view.frame.height - statusBarHeight
+        )
+        
+        // Delegate設定
+        tableView.delegate = self
+        
+        // DataSource設定
+        tableView.dataSource = self
+        
+        // 画面に UITableView を追加
+        self.view.addSubview(tableView)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // MARK: - UITableViewDataSource
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを作る
+        
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.accessoryType = .detailButton
+        cell.textLabel?.text = "セル\(indexPath.row + 1)"
+        cell.detailTextLabel?.text = "\(indexPath.row + 1)番目のセルの説明"
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // セルの数を設定
+        return 5
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // セルがタップされた時の処理
+        print("タップされたセルのindex番号: \(indexPath.row)")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // セルの高さを設定
+        return 64
+    }
+    
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        // アクセサリボタン（セルの右にあるボタン）がタップされた時の処理
+        print("タップされたアクセサリがあるセルのindex番号: \(indexPath.row)")
+    }
+}
